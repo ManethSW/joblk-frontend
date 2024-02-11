4dc2fa39c30f880857936a9f0215bd67f1a5ee4e
Maneth Weerasinghe <manethweerasinghe@gmail.com>
2024-02-11 19:54:30 +0530

    minor changes to handling avatar

--- app/components/Profile/Inputs/Avatar/Avatar.jsx
+++ app/components/Profile/Inputs/Avatar/Avatar.jsx
@@ -1,4 +1,4 @@
-import React, { createRef } from "react";
+import React, { createRef, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 import { Header, ActionButtonsDeleteAndSave, FormContainer } from "../Input";
@@ -6,51 +6,62 @@ import styles from "../Input.module.css";
 
 const AvtarInput = ({ avatar, setAvatar }) => {
   const fileInputRef = createRef();
+  const [avatarFile, setAvatarFile] = useState(null);
 
-  const handleAvatarClick = () => {
-    fileInputRef.current.click();
-  };
+const handleAvatarClick = () => {
+  console.log("Avatar clicked");
+  fileInputRef.current.click();
+};
 
-  const handleAvatarChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setAvatar(reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-    event.target.value = null;
-  };
+const handleAvatarChange = (event) => {
+  console.log("File input changed");
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setAvatar(reader.result);
+    };
+    reader.readAsDataURL(file);
+     // Store the file object
+    setAvatarFile(file);
+  }
+  event.target.value = null;
+};
 
-  const handleAvatarDeleteClick = () => {
-    setAvatar(null);
-    fileInputRef.current.value = null;
-  };
 
-  const handleAvatarSaveClick = async (e) => {
-    const newAvatar = JSON.stringify({ avatar });
-    e.preventDefault();
-    if (newAvatar != null) {
-      const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_API_USER}`;
-      const headers = {
-        auth_token: process.env.NEXT_PUBLIC_API_AUTH_TOKEN,
-      };
-      const data = {
-        newAvatar,
-      };
-      try {
-        await axios.put(url, data, {
-          headers,
-          withCredentials: true,
-        });
-      } catch (error) {
-        if (error.response && error.response.data) {
-          console.error(error.response.data["message"]);
-        }
+const handleAvatarDeleteClick = () => {
+  setAvatar(null);
+  setAvatarFile(null);
+  fileInputRef.current.value = null;
+};
+
+const handleAvatarSaveClick = async (e) => {
+  e.preventDefault();
+  console.log(avatarFile)
+  if (avatarFile) { // Check if there's a file object
+    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_API_USER}/avatar`;
+    const headers = {
+      auth_token: process.env.NEXT_PUBLIC_API_AUTH_TOKEN,
+    };
+
+    let formData = new FormData();
+    formData.append('avatar', avatarFile); // Append the file object to the FormData
+
+    try {
+      await axios.post(url, formData, {
+        headers: {
+          ...headers,
+          'Content-Type': 'multipart/form-data'
+        },
+        withCredentials: true,
+      });
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.error(error.response.data["message"]);
       }
     }
-  };
+  }
+};
 
   return (
     <FormContainer
@@ -70,6 +81,8 @@ const AvtarInput = ({ avatar, setAvatar }) => {
                 width={1}
                 height={1}
                 layout="responsive"
+                objectFit="cover"
+                objectPosition="center"
               />
             )}
           </div>
